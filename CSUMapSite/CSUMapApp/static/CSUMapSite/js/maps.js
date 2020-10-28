7a06e0fdec296fcfd8a2c43d114f759799e6da77
John K <jkoistinen6@gmail.com>
2020-10-28 14:49:35 -0700

    Add reference content

--- CSUMapSite/CSUMapApp/static/CSUMapSite/js/maps.js
+++ CSUMapSite/CSUMapApp/static/CSUMapSite/js/maps.js
@@ -12,3 +12,26 @@ function initMap() {
 function toggle() {
 	document.getElementById('sidebar').classList.toggle('collapsed');
 }
+
+function clubToggle() {
+	var coll = document.getElementById("club-content");
+	coll.classList.toggle("active");
+	if(coll.style.display == "none") {
+		coll.style.display = "block";
+	}
+	else {	
+		coll.style.display = "none";
+	}
+}
+
+/*
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });*/
