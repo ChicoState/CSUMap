f28925b0d32c850ae6001e6b35888b13bff3a21f
Noxium <brendanhanlon51@gmail.com>
2020-12-03 23:33:43 +0000

    Fix build errors

--- CSUMapSite/CSUMapApp/views.py
+++ CSUMapSite/CSUMapApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Building
+from . import forms
 
 def index(request):
     #endpoint = 'https://www.google.com/maps/embed/v1/view?key='
@@ -14,3 +15,29 @@ def index(request):
 
 def about(request):
 	return render(request, 'about.html')
+
+def home(request):
+	return render(request, 'index.html')
+
+def search(request):
+    if request.method == "POST":
+        mapFormIns = forms.mapForm(request.POST)
+        if mapFormIns.is_valid():
+            buildingN = mapFormIns.cleaned_data["buildingName"],
+            buildingLocation = buildingN[0]
+            buildingLatLng = buildingLocation.split(',');
+            print(buildingLatLng)
+            bLat =  buildingLatLng[0];
+            bLng =  buildingLatLng[1];
+    else:
+        mapFormIns = forms.mapForm()
+        bLat = ""
+        bLng = ""
+    print(bLng);
+    context ={
+         "bnlat" : bLat,
+         "bnlng" : bLng,
+         "formIns": mapFormIns
+    }
+    return render(request, 'search.html', context = context)
+
